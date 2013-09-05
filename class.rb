d54aafc345a6bbe6e5ae55217ab0bebc846c5f2e
Nik Daftary <nikdaftary@gmail.com>
2013-09-05 17:30:17 -0500

    translations and conversions

--- class.rb
+++ class.rb
@@ -1,4 +1,54 @@
-#Kernel.puts("Hellow World")
+
+
+puts 5.send(:*, 5)
+puts""
+
+puts "omg".send(:upcase)
+puts""
+
+puts ['a', 'b', 'c'].send(:at,1) 
+#['a', 'b', 'c'].at(1)
+puts""
+
+puts ['a', 'b', 'c'].send(:insert, 2, 'o', 'h', 'n', 'o' )
+#['a', 'b', 'c'].insert(2, 'o', 'h', 'n', 'o')
+puts""
+
+puts ({}.send(:size))
+#{}.size()
+puts ""
+
+##puts {character: "Mario"}.has_key?(:character)
+puts ({character:"Mario"}.send(:has_key?,:character))
+
+
+#-----Convert and remove Send method------------
+# 6.send(:-, 32)
+puts 6-32
+puts""
+
+puts ({html: true, json: false}.send(:keys))
+
+#{html: true, json: false}(keys:)
+
+
+
+# "MakerSquare".send(:*, :6)
+puts "MakerSquare"*6
+puts""
+
+# "MakerSquare".send(:split, 'a')
+puts "MakerSquare".split("a")
+
+# ['alpha', 'beta'].send(:[], 1)
+puts ["alpha", "beta"] [1]
+
+
+
+
+
+#---------------------------------------
+
 =begin
 Class Dog
 	def initialize()
@@ -24,7 +74,7 @@ Class Dog
 	end
 
 end
-=end
+
 
 
 class Marker
@@ -36,3 +86,5 @@ class Marker
 		puts "I'm writing with a #{color} marker! "
 	end
 end
+
+=end
